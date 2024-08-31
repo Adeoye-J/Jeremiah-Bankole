1d7aa4f2b3cd213fcd7d115e00feeb073a605b5a
Jeremiah <jeremiahiasngu@gmail.com>
2024-08-31 16:10:33 +0100

    hero now responsive

--- src/components/Home/Hero/Hero.jsx
+++ src/components/Home/Hero/Hero.jsx
@@ -1,17 +1,18 @@
 import React from 'react'
 import "./hero.css"
+import {FaWhatsapp, FaTwitter, FaLinkedin, FaEnvelope} from "react-icons/fa"
 
 const Hero = () => {
   return (
     <div className='hero-container flex'>
         <div className="hero-content flex">
+          <span>Keeping it simple!</span>
           <div className="text-section flex">
 
             <div className="basic-info flex">
-              <span>Keeping it simple!</span>
               <h3>Hello, I'm</h3>
-              <h1>Jeremiah Bankole</h1>
-              <h4>Let it stick</h4>
+              <h1>Jeremiah A. Bankole</h1>
+              <h4>Web Developer</h4>
             </div>
 
             <div className="action flex">
@@ -19,10 +20,25 @@ const Hero = () => {
               <button>Check them out here!</button>
             </div>
 
-          </div>
+            <div className="contact-link flex">
+              <div className="icon-con">
+                <FaWhatsapp className='icon' />
+              </div>
+              <div className="icon-con">
+                <FaTwitter className='icon' />
+              </div>
+              <div className="icon-con">
+                <FaLinkedin className='icon' />
+              </div>
+              <div className="icon-con">
+                <FaEnvelope className='icon' />
+              </div>
+            </div>
 
+          </div>
+          
           <div className="image-section">
-            <img src="/images/Jeremiah_Bankole.png" alt="Jeremiah" />
+            <img src="/images/Jeremiah_Bankole (2).png" alt="Jeremiah" />
           </div>
         </div>
     </div>
