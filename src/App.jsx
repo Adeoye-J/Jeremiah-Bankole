8dcfa930315c044eb0b29acd221a728b9d5cb740
Jeremiah <jeremiahiasngu@gmail.com>
2024-09-06 20:42:09 +0100

    including vercel analytics in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -6,12 +6,14 @@ import Skills from "./components/Skills/Skills"
 import Projects from "./components/Projects/Projects"
 import Contact from "./components/Contact/Contact"
 import Footer from "./containers/Footer/Footer"
+import { Analytics } from "@vercel/analytics/react"
 import './App.css'
 
 function App() {
 
   return (
     <>
+      <Analytics />
       <NavBar />
       <Routes>
         <Route path="/" element={<Home />} />
